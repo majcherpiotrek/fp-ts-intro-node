d8707087bf12210757889d2fb50d513d8ce625d8
majcherpiotrek <piotr.majcher@softwaremill.com>
2022-10-10 16:42:03 +0100

    traverse array and collect errors

--- either.test.ts
+++ either.test.ts
@@ -1,6 +1,14 @@
-import { safeParseFloat, safeParseInt } from './either';
+import {
+  safeParseFloat,
+  safeParseInt,
+  traverseAndCollectErrors,
+} from './either';
 
 import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as NonEmptyArray from 'fp-ts/NonEmptyArray';
+import { pipe } from 'fp-ts/function';
 
 describe('either', () => {
   it('safeParseFloat', () => {
@@ -14,5 +22,73 @@ describe('either', () => {
     expect(safeParseInt('')).toEqual(E.left("'' is not a number!"));
     expect(safeParseInt('1.5')).toEqual(E.left("'1.5' is not an Integer!"));
     expect(safeParseInt('150')).toEqual(E.right(150));
+  });
+
+  it('traverseArray', () => {
+    const evenFilter = jest.fn(
+      E.fromPredicate<number, number, string>(
+        (n): n is number => n % 2 === 0,
+        () => 'odd'
+      )
+    );
+
+    const arr = [2, 4, 5, 6];
+
+    const res = pipe(arr, E.traverseArray(evenFilter));
+
+    expect(res).toEqual(E.left('odd'));
+    expect(evenFilter).toHaveBeenCalledTimes(3);
+    expect(evenFilter).toHaveBeenLastCalledWith(5);
+  });
+
+  it('traverseArray TE', async () => {
+    const evenFilter = jest.fn(
+      TE.fromPredicate<string, number, number>(
+        (n): n is number => n % 2 === 0,
+        () => 'odd'
+      )
+    );
+
+    const arr = [2, 4, 5, 6];
+
+    const res = await pipe(arr, TE.traverseArray(evenFilter))();
+
+    expect(res).toEqual(E.left('odd'));
+    expect(evenFilter).toHaveBeenCalledTimes(4);
+    expect(evenFilter).toHaveBeenLastCalledWith(6);
+  });
+
+  it('traverseAndCollectErrors should fail on 3 and 5', async () => {
+    const arr = [2, 3, 5, 6];
+
+    const evenFilter = jest.fn(
+      TE.fromPredicate<string, number, number>(
+        (n): n is number => n % 2 === 0,
+        (n) => `${n} is odd`
+      )
+    );
+
+    const res = await traverseAndCollectErrors(evenFilter)(arr)();
+
+    expect(res).toEqual(E.left(['3 is odd', '5 is odd']));
+    expect(evenFilter).toHaveBeenCalledTimes(4);
+    expect(evenFilter).toHaveBeenLastCalledWith(6);
+  });
+
+  it('traverseAndCollectErrors should succeed', async () => {
+    const arr = [2, 4, 6, 8];
+
+    const evenFilter = jest.fn(
+      TE.fromPredicate<string, number, number>(
+        (n): n is number => n % 2 === 0,
+        (n) => `${n} is odd`
+      )
+    );
+
+    const res = await traverseAndCollectErrors(evenFilter)(arr)();
+
+    expect(res).toEqual(E.right([2, 4, 6, 8]));
+    expect(evenFilter).toHaveBeenCalledTimes(4);
+    expect(evenFilter).toHaveBeenLastCalledWith(8);
   });
 });
--- either.ts
+++ either.ts
@@ -1,6 +1,9 @@
 import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import * as T from 'fp-ts/Task';
 import { flow, pipe } from 'fp-ts/lib/function';
-import * as A from 'fp-ts/ReadonlyArray';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as NonEmptyArray from 'fp-ts/NonEmptyArray';
 /* Option is great for dealing with nullable values, but it might not be enough when we want to store the information about the error - in other words, know what went wrong inside a chain of optional operations. In this situation Either comes to the rescue! */
 
 type Integer = number & { _tag: 'Integer' };
@@ -43,9 +46,9 @@ interface OperationError {
 export const powList = (list: readonly unknown[]) =>
   pipe(
     list,
-    A.map(safeParseInt),
-    A.map(E.map(pow2)),
-    A.map(E.mapLeft<string, OperationError>((error) => ({ error })))
+    RA.map(safeParseInt),
+    RA.map(E.map(pow2)),
+    RA.map(E.mapLeft<string, OperationError>((error) => ({ error })))
   );
 
 // The problem here is that we get a list of eithers as a result, which is difficult to work with - we would like to get an error if any of the list elements fails
@@ -56,6 +59,16 @@ export const powListTraverse = (list: readonly unknown[]) =>
   pipe(
     list,
     E.traverseArray(safeParseInt),
-    E.map(A.map(pow2)),
+    E.map(RA.map(pow2)),
     E.mapLeft<string, OperationError>((error) => ({ error }))
   );
+
+const ApplicativeTE = <E>() =>
+  TE.getApplicativeTaskValidation(
+    T.ApplyPar,
+    pipe(NonEmptyArray.getSemigroup<E>())
+  );
+
+export const traverseAndCollectErrors = <E, A, B>(
+  fn: (a: A) => TE.TaskEither<E, B>
+) => RA.traverse(ApplicativeTE<E>())(flow(fn, TE.mapLeft(NonEmptyArray.of)));
